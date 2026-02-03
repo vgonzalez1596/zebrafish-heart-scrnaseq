f6b994111e15bc46eb24f3af1dcca459fb9daa7f
Vanessa Gonzalez <vgonzalez1596@gmail.com>
2026-02-03 01:01:56 -0500

    Update 07_fluorophore_validation.R

--- scripts/07_fluorophore_validation.R
+++ scripts/07_fluorophore_validation.R
@@ -65,6 +65,7 @@ heart16s_final <- RunUMAP(heart16s_filtered_normalized_PCA13_res0.12, dims = 1:1
 #Save final fluorophore dataset
 saveRDS(heart16s_final, file = file.path(results_dir, "heart16s_fluorophore_final.rds"))
 
+# Feature plots for ZsYellow and nkx2.5
 blend_plot_16s <- FeaturePlot(
   heart16s_final,
   features = c("ZsYellow", "nkx2.5"),
@@ -74,124 +75,76 @@ blend_plot_16s <- FeaturePlot(
 blend_plot_16s[[1]] <- blend_plot_16s[[1]] + ggtitle("zsyellow") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_16s[[2]] <- blend_plot_16s[[2]] + ggtitle("nkx2.5") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_16s[[3]] <- blend_plot_16s[[3]] + ggtitle("zsyellow and nkx2.5") + theme(plot.title = element_text(face = "bold.italic"))
-blend_plot_16s[[4]] <- blend_plot_16s[[4]] +
-  labs(x = "zsyellow", y = "nkx2.5") +
-  theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
-
+blend_plot_16s[[4]] <- blend_plot_16s[[4]] + labs(x = "zsyellow", y = "nkx2.5") + theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
 p_16 <- blend_plot_16s[[1]] + blend_plot_16s[[2]] + blend_plot_16s[[3]] + blend_plot_16s[[4]] + plot_layout(ncol = 4)
-
 ggsave(
   filename = file.path(figures_dir, "16ss_fluorophore_blend_ZsYellow_nkx2p5.png"),
   plot = p_16,
   width = 14, height = 4, dpi = 300
 )
 
-# -------------------------------#
-# 2) 18 ss: ZsYellow + nkx2.5
-# -------------------------------#
-
-heart18s <- CreateSeuratObject(
-  counts = heart18s_fluoro.data,
-  project = "18somdata_fluorophore",
-  min.cells = 3,
-  min.features = 200
-)
-
+# Seurat analysis, standard workflow, 18 ss fluorophore dataset 
+heart18s <- CreateSeuratObject(counts = heart18s.data, project = "18somdata", min.cells = 3, min.features = 200)
 heart18s[["percent.mt"]] <- PercentageFeatureSet(heart18s, pattern = "^mt-")
-
-heart18s_filtered <- subset(
-  heart18s,
-  subset = nFeature_RNA > 200 &
-    nFeature_RNA < 7974 &
-    percent.mt < 7.79 &
-    nCount_RNA < 100000
-)
-
-heart18s_filtered <- NormalizeData(heart18s_filtered, normalization.method = "LogNormalize", scale.factor = 10000)
-heart18s_filtered <- FindVariableFeatures(heart18s_filtered, selection.method = "vst", nfeatures = 2000)
-
-all.genes <- rownames(heart18s_filtered)
-heart18s_filtered <- ScaleData(heart18s_filtered, features = all.genes)
-heart18s_filtered <- RunPCA(heart18s_filtered, features = VariableFeatures(object = heart18s_filtered))
-
-heart18s_filtered <- FindNeighbors(heart18s_filtered, dims = 1:16)
-heart18s_filtered <- FindClusters(heart18s_filtered, resolution = 0.15)
-heart18s_final <- RunUMAP(heart18s_filtered, dims = 1:16)
-
+heart18s_filtered <- subset(heart18s, subset = nFeature_RNA > 200 & nFeature_RNA < 7974 & percent.mt < 7.79 & nCount_RNA < 100000)
+heart18s_filtered_normalized <- NormalizeData(heart18s_filtered, normalization.method = "LogNormalize", scale.factor = 10000)
+heart18s_filtered_normalized <- FindVariableFeatures(heart18s_filtered_normalized, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(heart18s_filtered_normalized), 10)
+all.genes <- rownames(heart18s_filtered_normalized)
+heart18s_filtered_normalized <- ScaleData(heart18s_filtered_normalized, features = all.genes)
+heart18s_filtered_normalized <- RunPCA(heart18s_filtered_normalized, features = VariableFeatures(object = heart18s_filtered_normalized))
+heart18s_filtered_normalized_PCA16 <- FindNeighbors(heart18s_filtered_normalized, dims = 1:16)
+heart18s_filtered_normalized_PCA16_res0.15 <- FindClusters(heart18s_filtered_normalized_PCA16, resolution = .15)
+heart18s_final <- RunUMAP(heart18s_filtered_normalized_PCA16_res0.15, dims = 1:16)
+#Save final fluorophore dataset
 saveRDS(heart18s_final, file = file.path(results_dir, "heart18s_fluorophore_final.rds"))
 
+# Feature plots for ZsYellow and nkx2.5
 blend_plot_18s <- FeaturePlot(
   heart18s_final,
   features = c("ZsYellow", "nkx2.5"),
   blend = TRUE,
   cols = c("#D41159", "#1A85FF")
 )
-
 blend_plot_18s[[1]] <- blend_plot_18s[[1]] + ggtitle("zsyellow") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_18s[[2]] <- blend_plot_18s[[2]] + ggtitle("nkx2.5") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_18s[[3]] <- blend_plot_18s[[3]] + ggtitle("zsyellow and nkx2.5") + theme(plot.title = element_text(face = "bold.italic"))
-blend_plot_18s[[4]] <- blend_plot_18s[[4]] +
-  labs(x = "zsyellow", y = "nkx2.5") +
-  theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
-
+blend_plot_18s[[4]] <- blend_plot_18s[[4]] + labs(x = "zsyellow", y = "nkx2.5") + theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
 p_18 <- blend_plot_18s[[1]] + blend_plot_18s[[2]] + blend_plot_18s[[3]] + blend_plot_18s[[4]] + plot_layout(ncol = 4)
-
 ggsave(
   filename = file.path(figures_dir, "18ss_fluorophore_blend_ZsYellow_nkx2p5.png"),
   plot = p_18,
   width = 14, height = 4, dpi = 300
 )
 
-# -------------------------------#
-# 3) 20 ss: EGFP + myl7
-# -------------------------------#
-
-heart20s <- CreateSeuratObject(
-  counts = heart20s_fluoro.data,
-  project = "20somdata_fluorophore",
-  min.cells = 3,
-  min.features = 200
-)
-
+# Seurat analysis, standard workflow, 20 ss fluorophore dataset 
+heart20s <- CreateSeuratObject(counts = heart20s.data, project = "20somdata", min.cells = 3, min.features = 200)
 heart20s[["percent.mt"]] <- PercentageFeatureSet(heart20s, pattern = "^mt-")
-
-heart20s_filtered <- subset(
-  heart20s,
-  subset = nFeature_RNA > 200 &
-    nFeature_RNA < 5820 &
-    percent.mt < 5.22 &
-    nCount_RNA < 100000
-)
-
-heart20s_filtered <- NormalizeData(heart20s_filtered, normalization.method = "LogNormalize", scale.factor = 10000)
-heart20s_filtered <- FindVariableFeatures(heart20s_filtered, selection.method = "vst", nfeatures = 2000)
-
-all.genes <- rownames(heart20s_filtered)
-heart20s_filtered <- ScaleData(heart20s_filtered, features = all.genes)
-heart20s_filtered <- RunPCA(heart20s_filtered, features = VariableFeatures(object = heart20s_filtered))
-
-heart20s_filtered <- FindNeighbors(heart20s_filtered, dims = 1:13)
-heart20s_filtered <- FindClusters(heart20s_filtered, resolution = 0.2)
-heart20s_final <- RunUMAP(heart20s_filtered, dims = 1:13)
-
+heart20s_filtered <- subset(heart20s, subset = nFeature_RNA > 200 & nFeature_RNA < 5820 & percent.mt < 5.22 & nCount_RNA < 100000)
+heart20s_filtered_normalized <- NormalizeData(heart20s_filtered, normalization.method = "LogNormalize", scale.factor = 10000)
+heart20s_filtered_normalized <- FindVariableFeatures(heart20s_filtered_normalized, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(heart20s_filtered_normalized), 10)
+all.genes <- rownames(heart20s_filtered_normalized)
+heart20s_filtered_normalized <- ScaleData(heart20s_filtered_normalized, features = all.genes)
+heart20s_filtered_normalized <- RunPCA(heart20s_filtered_normalized, features = VariableFeatures(object = heart20s_filtered_normalized))
+heart20s_filtered_normalized_PCA13 <- FindNeighbors(heart20s_filtered_normalized, dims = 1:13)
+heart20s_filtered_normalized_PCA13_res0.2 <- FindClusters(heart20s_filtered_normalized_PCA13, resolution = .2)
+heart20s_final <- RunUMAP(heart20s_filtered_normalized_PCA13_res0.2, dims = 1:13)
+#Save final fluorophore dataset
 saveRDS(heart20s_final, file = file.path(results_dir, "heart20s_fluorophore_final.rds"))
 
+# Feature plots for EGFP and myl7
 blend_plot_20s <- FeaturePlot(
   heart20s_final,
   features = c("EGFP", "myl7"),
   blend = TRUE,
   cols = c("#D41159", "#1A85FF")
 )
-
 blend_plot_20s[[1]] <- blend_plot_20s[[1]] + ggtitle("egfp") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_20s[[2]] <- blend_plot_20s[[2]] + ggtitle("myl7") + theme(plot.title = element_text(face = "bold.italic"))
 blend_plot_20s[[3]] <- blend_plot_20s[[3]] + ggtitle("egfp and myl7") + theme(plot.title = element_text(face = "bold.italic"))
-blend_plot_20s[[4]] <- blend_plot_20s[[4]] +
-  labs(x = "egfp", y = "myl7") +
-  theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
-
+blend_plot_20s[[4]] <- blend_plot_20s[[4]] + labs(x = "egfp", y = "myl7") + theme(axis.title.x = element_text(face = "italic"), axis.title.y = element_text(face = "italic"))
 p_20 <- blend_plot_20s[[1]] + blend_plot_20s[[2]] + blend_plot_20s[[3]] + blend_plot_20s[[4]] + plot_layout(ncol = 4)
-
 ggsave(
   filename = file.path(figures_dir, "20ss_fluorophore_blend_EGFP_myl7.png"),
   plot = p_20,
@@ -199,4 +152,3 @@ ggsave(
 )
 
 message("07_fluorophore_validation.R complete. Outputs saved to results/fluorophore_validation and figures/fluorophore_validation.")
-
